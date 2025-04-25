cffaa83d3406648f3e5d7bf3dd68203655a6d545
Kiino007 <kino007kevin@gmail.com>
2025-04-25 18:52:09 -0500

    Clase4
    Inicio de clase

---
+++ Clase_4.py
@@ -0,0 +1,5 @@
+# Herencia simple...
+#Es la capacidad de una subclase que hereda atributos y metodos de una clase padre.
+
+# class Subclase(ClasePadre):
+#print("Soy una subclase que hereda de una clase padre.")
