e92decd129c3e7115ce3f4c6c18dbebfedc67a35
Kiino007 <kino007kevin@gmail.com>
2025-04-25 19:29:11 -0500

    Clase4
    Primer ejemplo

--- Clase_4.py
+++ Clase_4.py
@@ -3,3 +3,11 @@
 
 # class Subclase(ClasePadre):
 #print("Soy una subclase que hereda de una clase padre.")
+
+#Definamos que atributos o metodos vamos a llamar de la clase padre con la palabra reservada super(), que directamente llama al contrusctor para inicializar o complementar comportamientos
+
+#sobreescritura de metodos
+#concepto: una subclase puede redefinir (override) un metodo heredado para adaptar o ampliar su funcionalidad
+
+#como hacerlo: definiendo en la subclase un metodo con el mismo nombre, se puede usar super().metodo() dentro de la nueva implementacion
+
